641cc9e07beb309aa2e159d7c758672ca010b3ef
nickkim79 <nick.kim@productplan.com>
2019-02-12 19:57:23 -0800

    updated project to use new template

--- src/App.vue
+++ src/App.vue
@@ -64,117 +64,69 @@
 </template>
 
 <script>
-    import * as math from 'mathjs';
-    export default {
-        name: "app",
-        data() {
-            return {
-                numPeople: 1,
-                hidePeopleField: true,
-                hideTipField: true,
-                tip: 15,
-                billTotal: 0,
-                tipPerPerson: 0,
-                totalPerPerson: 0,
-                activePeopleButton: 0,
-                activeTipButton: 0,
-                hideResults: true,
-            };
+import * as math from 'mathjs';
+
+var data = {
+    numPeople: 1,
+    hidePeopleField: true,
+    hideTipField: true,
+    tip: 15,
+    billTotal: 0,
+    tipPerPerson: 0,
+    totalPerPerson: 0,
+    activePeopleButton: 0,
+    activeTipButton: 0,
+    hideResults: true,
+};
+export default {
+    methods: {
+        calculate() {
+            this.tipPerPerson = math.round(math.eval((this.billTotal * (this.tip / 100)) / this.numPeople), 2);
+            this.totalPerPerson = math.round(math.eval((this.billTotal / this.numPeople) + this.tipPerPerson), 2);
+            this.hideResults = false;
         },
-        methods: {
-            calculate() {
-                this.tipPerPerson = math.round(math.eval((this.billTotal * (this.tip / 100)) / this.numPeople), 2);
-                this.totalPerPerson = math.round(math.eval((this.billTotal / this.numPeople) + this.tipPerPerson), 2);
-                this.hideResults = false;
-            },
-            setNumPeople(people) {
-                this.activePeopleButton = 0;
-                this.hidePeopleField = true;
-                this.activePeopleButton = people;
-                this.numPeople = people;
-            },
-            showCustomNumPeople() {
-                this.activePeopleButton = 0;
-                this.activePeopleButton = 5;
-                this.hidePeopleField = false;
-            },
-            showCustomTip() {
-                this.activeTipButton = 0;
-                this.activeTipButton = 4;
-                this.hideTipField = false;
-            },
-            setTip(tip) {
-                this.activeTipButton = 0;
-                this.hideTipField = true;
-                switch (tip) {
-                    case 15:
-                        this.activeTipButton = 1;
-                        this.tip = tip;
-                        break;
-                    case 18:
-                        this.activeTipButton = 2;
-                        this.tip = tip;
-                        break;
-                    case 20:
-                        this.activeTipButton = 3;
-                        this.tip = tip;
-                        break;
-                    default:
-                        this.activeTipButton = 4;
-                        this.tip = tip;
-                        break;
-                }
+        setNumPeople(people) {
+            this.activePeopleButton = 0;
+            this.hidePeopleField = true;
+            this.activePeopleButton = people;
+            this.numPeople = people;
+        },
+        showCustomNumPeople() {
+            this.activePeopleButton = 0;
+            this.activePeopleButton = 5;
+            this.hidePeopleField = false;
+        },
+        showCustomTip() {
+            this.activeTipButton = 0;
+            this.activeTipButton = 4;
+            this.hideTipField = false;
+        },
+        setTip(tip) {
+            this.activeTipButton = 0;
+            this.hideTipField = true;
+            switch (tip) {
+                case 15:
+                    this.activeTipButton = 1;
+                    this.tip = tip;
+                    break;
+                case 18:
+                    this.activeTipButton = 2;
+                    this.tip = tip;
+                    break;
+                case 20:
+                    this.activeTipButton = 3;
+                    this.tip = tip;
+                    break;
+                default:
+                    this.activeTipButton = 4;
+                    this.tip = tip;
+                    break;
             }
         }
-    };
+    },
+    data: () => data
+};
 </script>
-
-<style>
-  body {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background: #f5f5f5;
-  }
-
-  div {
-    padding: 10px 0;
-  }
-
-  a.button:hover {
-    background-color: #b9ca4d;
-  }
-
-  .calculator {
-    width: 425px;
-    margin: 20px auto;
-    padding: 35px;
-    background-color: #fff;
-  }
-
-  .hide {
-    display: none;
-  }
-
-  .selected {
-    background-color: #cab34d
-  }
-
-  .flex {
-    display: flex;
-    justify-content: space-between;
-    align-content: center;
-    width: 80%;
-  }
-
-  input.total {
-    width: 50%;
-  }
-
-  input.people {
-    width: 50%;
-  }
-
-  input.tip {
-    width: 50%;
-  }
+<style lang="less">
+  @import "style.less";
 </style>
